e9e0a555d45ea85a5bec1973831304e9f2647587
junkyeom <junkyeom0203@naver.com>
2024-10-20 23:59:27 +0900

    write title 제작

--- app/write/editor.js
+++ app/write/editor.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import 'react-quill/dist/quill.snow.css';
 import './main.css'
@@ -9,15 +9,42 @@ const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 export default function Editor() {
   const [content, setContent] = useState('');
-  
-    const handleChange = (value) => {
-      setContent(value);
-    };
 
+  const modules = {
+    toolbar: [
+      [{ header: '1' }, { header: '2' }],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['bold', 'italic', 'underline'],
+      [{ color: [] }, { background: [] }],
+      ['link', 'image'],
+      [{ align: [] }],
+      ['clean'],
+    ],
+  };
+
+  // Formats allowed in the editor
+  const formats = [
+    'header',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'list',
+    'bullet',
+    'link',
+    'image',
+    'color',
+    'background',
+    'align',
+  ];
+
+  const handleChange = (value) => {
+    setContent(value);
+  };
 
     return (
       <div className='editor'>
-        <ReactQuill value={content} onChange={handleChange}/>
+        <ReactQuill value={content} modules={modules} formats={formats} onChange={handleChange} />
       </div>
     )
   }
--- app/write/page.js
+++ app/write/page.js
@@ -4,7 +4,23 @@ import Editor from './editor';
 export default function Write() {
 
     return (
-      <div className='write'>
+      <div className='write-page'>
+        <h5>글 작성</h5>
+        <div className='write-header'>
+          <div className='ctg-container'>
+            <h5 className='ctg-title'>카테고리</h5>
+            <select id='category'>
+              <option value='null'>분류없음</option>
+              <option value='nomal'>일반</option>
+              <option value='review'>후기</option>
+              <option value='ad'>AD</option>
+            </select>
+          </div>
+          <div className='title-container'>
+            <h5>제목</h5>
+            <input type='text' placeholder='제목을 입력해 주세요'></input>
+          </div>
+        </div>
         <Editor/>
       </div>
     )
