ffaaebb61c5b71e84b8d1ca7afff0c8fdaa15007
junkyeom <junkyeom0203@naver.com>
2024-11-02 01:35:47 +0900

    독서포럼 완성

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -9,9 +9,13 @@ export default async function Detail(props){
     return (
         <div className="detail-page">
             <div className="detail-box">
-                <h4>상세페이지</h4>
-                <h4>{result.title}</h4>
-                <CleanedResult content={result.content}/>
+                <div id='detail-header'>
+                    <p id='detail-type'>{result.type}</p>
+                    <h1 id='detail-title'>{result.title}</h1>
+                </div>
+                <div id='detail-content'>
+                    <CleanedResult content={result.content}/>
+                </div>
             </div>
         </div>
     )
