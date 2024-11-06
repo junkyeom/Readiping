94c10e362179e7c2e081162beb3f6b8cf8e66026
junkyeom <junkyeom0203@naver.com>
2024-11-06 17:19:38 +0900

    표 디자인 시작

--- app/reading/ReadingItem.js
+++ app/reading/ReadingItem.js
@@ -19,10 +19,10 @@ export default function ReadingItem({result}) {
             <table className="table">
                 <thead>
                     <tr>
-                        <th>번호</th>
-                        <th>카테고리</th>
-                        <th>제목</th>
-                        <th>글쓴이</th>
+                        <th width='8%'>번호</th>
+                        <th width='10%'>카테고리</th>
+                        <th width='60%'>제목</th>
+                        <th width='10%'>글쓴이</th>
                         <th>날짜</th>
                     </tr>   
                 </thead>
@@ -32,7 +32,7 @@ export default function ReadingItem({result}) {
                         <tr className='table-list' key={keys-i}>
                             <td>{keys-i}</td>
                             <td>{a.type}</td>
-                            <td><Link href={'/detail/'+a._id.toString()}>{a.title}</Link></td>
+                            <td className="td-title"><Link href={'/detail/'+a._id.toString()}>{a.title}</Link></td>
                             <td>{a.name}</td>
                             <td>{a.time}</td>
                         </tr>
