1b96c5b7620af5c9a347c1dd6f3d204597c695c5
junkyeom <junkyeom0203@naver.com>
2024-11-23 17:37:39 +0900

    회원가입 디자인 완료

--- app/register/page.js
+++ app/register/page.js
@@ -5,24 +5,24 @@ export default function Register() {
       <div className="register-page">
         <h3>회원가입</h3>
           <form method="POST" action="/api/auth/signup">
-            <table>
+            <table className='reg-table'>
               <tbody>
                 <tr>
-                  <th>닉네임</th>
-                  <td>
+                  <th className='reg-th'>닉네임</th>
+                  <td className='reg-td'>
                     <input id='nick-input' name="name" type="text"/>
                   </td>
                 </tr>
                 <tr>
-                  <th>아이디</th>
-                  <td className='tdtd'>
+                  <th className='reg-th'>아이디</th>
+                  <td className='reg-td'>
                     <input name="id" type="text"/>
                     <p className='reg-warning'>영문 또는 숫자를 포함한 6-20자</p>
                   </td>
                 </tr>
                 <tr>
-                  <th>패스워드</th>
-                  <td>
+                  <th className='reg-tb'>패스워드</th>
+                  <td className='reg-td'>
                     <input name="password" type="password"/>
                     <p className='reg-warning'>8 ~ 16자 영문, 숫자, 특수문자를 최소 한가지씩 조합</p>
                   </td>
