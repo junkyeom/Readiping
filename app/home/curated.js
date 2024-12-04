db456123c21f3e229ae28ffa9267c333b41adb4c
junkyeom <junkyeom0203@naver.com>
2024-12-04 19:24:12 +0900

    curated 완성

--- app/home/curated.js
+++ app/home/curated.js
@@ -8,25 +8,37 @@ export default function Curated() {
     return (
         <div className="curated">
             <div className="curated-container">
-            <div>
-                <Link href='detail/674ffba5ecc611c13b338b4d'>
-                    <div style={{width : '445px', height : 'auto'}}>
-                        <Image 
-                            src='/images/crated-1.jpg'
-                            alt="Example image" 
-                            fill
-                        />
-                    </div>
-                </Link>
-                </div>
                 <div>
-                    <Link href='detail/674ffba5ecc611c13b338b4d'>
-                        <div style={{width : '100%', height : 'auto'}}>
+                    <Link href='/detail/674ffba5ecc611c13b338b4d' className="curated-link">
+                        <div className="curated-item">
                             <Image 
-                                src='/images/crated-1.jpg'
+                                src="/images/curated-2.jpeg"
                                 alt="Example image" 
                                 fill
+                                objectFit="cover"
                             />
+                        </div>
+                        <div className="curated-content">
+                            <p style={{margin : '0', color : '#333', fontWeight : '700'}}>
+                                BOUND X IGOR
+                            </p>
+                        </div>
+                    </Link>
+                </div>
+                <div>
+                    <Link href='/detail/674ffba5ecc611c13b338b4d' className="curated-link">
+                        <div className="curated-item">
+                            <Image 
+                                src='/images/curated-1.jpg'
+                                alt="Example image" 
+                                fill
+                                objectFit="cover"
+                            />
+                        </div>
+                        <div className="curated-content">
+                            <p style={{margin : '0', color : '#333', fontWeight : '700'}}>
+                                키에르케고르 &lt;죽음에 관하여&gt; 리뷰
+                            </p>
                         </div>
                     </Link>
                 </div>
