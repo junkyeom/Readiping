12f59d4fb97bb8128b2e52e3ba01315c7fa0a0d1
junkyeom <junkyeom0203@naver.com>
2025-01-16 10:12:23 +0900

    홈페이지 배너 크기 조절

--- app/page.js
+++ app/page.js
@@ -32,6 +32,7 @@ export default function Home() {
         <Image
           src="/images/banner/home-1.png" 
           alt="description" 
+          layout="responsive"
           width={1020}
           height={136}
           style={{ borderRadius: '10px' }}
